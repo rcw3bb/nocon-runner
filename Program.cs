cb421147c0d40cbb538f61e4871b3503e43feb76
ron <ron@ronella.xyz>
2019-10-18 23:17:34 +1300

    Implement configuration support.

--- Program.cs
+++ Program.cs
@@ -1,35 +1,63 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace nocon_runner
 {
+
+    class NoConJson {
+        public string Command {get; set;}
+
+        public List<string> Arguments {get; set;}
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             if (args.Length==0) {
-                Console.WriteLine("Usage: nocon-runner <COMMAND> [<ARG1>[ <ARG2>[ <ARGN>]]]");
-                return;
+                var confFile = new FileInfo(Path.Join(".", "conf", "nocon.json"));
+                if (confFile.Exists) {
+                    var confText = File.ReadAllText(confFile.FullName);
+                    var confJson = JsonConvert.DeserializeObject<NoConJson>(confText);
+                    var confCommand = confJson.Command;
+                    var confArguments = confJson.Arguments;
+                    List<string> confData = new List<string>();
+
+                    if (null!=confCommand) {
+                        confData.Add(confCommand);
+                        if (null!=confArguments) {
+                            confArguments.ForEach(  ___arg => {
+                                confData.Add(___arg);
+                            });
+                        }
+                    }
+
+                    if (confData.Count > 0) {
+                        args = confData.ToArray();
+                    }
+                }
+                else {
+                    Console.WriteLine("Usage: nocon-runner <COMMAND> [<ARG1>[ <ARG2>[ <ARGN>]]]");
+                    return;
+                }
             }
 
             var command = args[0];
             string[] arguments = {};
 
-            StringBuilder sbCommand = new StringBuilder(command);
-
             if (args.Length > 1) {
                 arguments = new string[args.Length - 1];
                 Array.Copy(args, 1, arguments, 0, arguments.Length);
-                sbCommand.Append(" ");
-                sbCommand.Append(String.Join(" ", arguments));
             }
 
             Process process = new Process();
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.CreateNoWindow = true;
             startInfo.FileName = command;
-            startInfo.Arguments = string.Join(" ", args);
+            startInfo.Arguments = string.Join(" ", arguments);
             process.StartInfo = startInfo;
             process.Start();
         }
